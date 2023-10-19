d7e633b255c311d5a87709ffd65e92fe7966942e
Airamek <vegkeppnemairamek@gmail.com>
2023-10-19 09:25:24 +0200

    Add project files.

---
+++ SZGYA-Helsinki-20231019/Helyezes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_Helsinki_20231019
+{
+    class Helyezes
+    {
+        private static Dictionary<int,int> pontTabla = new Dictionary<int, int>() {
+            {1, 7},
+            {2, 5},
+            {3, 4},
+            {4, 3},
+            {5, 2},
+            {6, 1}
+        };
+
+        public int ElertHely { get; set; }
+        public int SportolokSzama { get; set; }
+        public string Sportag { get; set; }
+        public string Versenyszam { get; set; }
+
+        public int OlimpiaiPont
+        {
+            get
+            {
+                if (ElertHely > 6) return 0;
+                else return Helyezes.pontTabla[ElertHely];
+            }
+        }
+
+        public Helyezes(string sor)
+        {
+            string[] adatok = sor.Split(' ');
+            this.ElertHely = int.Parse(adatok[0]);
+            this.SportolokSzama = int.Parse(adatok[1]);
+            this.Sportag = adatok[2];
+            this.Versenyszam = adatok[3];
+        }
+
+        public override string ToString()
+        {
+            return $"{this.ElertHely} {this.SportolokSzama} {this.OlimpiaiPont} {this.Sportag} {this.Versenyszam}";
+        }
+    }
+}
---
+++ SZGYA-Helsinki-20231019/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using SZGYA_Helsinki_20231019;
+
+namespace SZGYAHelsinki
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Helyezes> helyezesek = new List<Helyezes>();
+
+            using StreamReader sr = new StreamReader("../../../src/helsinki.txt");
+            while (!sr.EndOfStream) 
+            {
+                helyezesek.Add(new Helyezes(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"3.feladat: \nPontszerző helyezések száma: {helyezesek.Count}");
+
+            var helyezesKat = helyezesek
+                .GroupBy(h => h.ElertHely)
+                .ToDictionary(k => k.Key, v => v.Count());
+            Console.WriteLine("4.feladat:");
+            Console.WriteLine($"Arany: {helyezesKat[1]}");
+            Console.WriteLine($"Ezüst: {helyezesKat[2]}");
+            Console.WriteLine($"Bronz: {helyezesKat[3]}");
+            Console.WriteLine($"Összesen: {helyezesKat.Where(h => h.Key <= 3).Sum(h => h.Value)}");
+
+            Console.WriteLine("5.feladat:");
+            Console.WriteLine($"Olimpiai pontok száma {helyezesek.Sum(h => h.OlimpiaiPont)}");
+
+            Console.WriteLine("6.feladat:");
+            var helyezesekAgSzerint = helyezesek.GroupBy(h => h.Sportag).ToDictionary(k => k.Key, v => v.Count());
+            if (helyezesekAgSzerint["torna"] > helyezesekAgSzerint["uszas"])
+                Console.WriteLine("Torna sportágban szereztek több érmet.");
+            else if (helyezesekAgSzerint["torna"] < helyezesekAgSzerint["uszas"])
+                Console.WriteLine("Úszás sportágban szereztek több érmet.");
+            else Console.WriteLine("Egyenlp volt az érmek száma");
+
+            for (int i = 0; i < helyezesek.Count; i++)
+            {
+                if (helyezesek[i].Sportag == "kajakkenu") helyezesek[i].Sportag = "kajak-kenu";
+            }
+
+            using StreamWriter sw = new StreamWriter("../../../src/helsinki2.txt");
+            foreach(var helyezes in helyezesek)
+            {
+                sw.WriteLine(helyezes);
+            }
+
+            Console.WriteLine("8.feladat:");
+            var legtobbSportolo = helyezesek.MaxBy(h => h.SportolokSzama);
+            Console.WriteLine($"Helyezés: {legtobbSportolo.ElertHely}");
+            Console.WriteLine($"Sportág: {legtobbSportolo.Sportag}");
+            Console.WriteLine($"Versenyzám: {legtobbSportolo.Versenyszam}");
+            Console.WriteLine($"Sportolók száma: {legtobbSportolo.SportolokSzama}");
+
+        }
+    }
+}
